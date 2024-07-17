3e1ac27b44b1ae8681162d76dd80c9666fef9199
Real <adrianusvir@gmail.com>
2024-07-17 21:16:54 +0200

    Update bg.lua

--- bg.lua
+++ bg.lua
@@ -4,7 +4,8 @@
 --- IAC/bg.lua
 --- BLACKLISTED GROUPS
 
-
 local blacklistedgroups = {
-   [9327275] -- Zoger SCPF
+   9327275, -- Zoger SCPF
+   1234567, -- Example Group
 }
+return blacklistedgroups
