34be7aef27c5722f95af4cb329d7ff7a5f7a2721
Real <adrianusvir@gmail.com>
2024-07-17 22:37:31 +0200

    Update sa.lua

--- sa.lua
+++ sa.lua
@@ -6,10 +6,22 @@
 
 
 local Reports = {
-  alternative = {  
+  AlternativeAccount = {  
    1885538965, -- Roblox Username: AmateurMastered | Alternative Account of the CelesticialFlaw. | Reason: Literally alt.
+   1874723110, -- Roblox Username: TheReaICalciumm | Alternative & Main of Demise/InyxReborn/hxrrsilence Account. | Reason: Agent and his account.
+   4067202176, -- Roblox Username: InyxReborn | Alternative & Main of Demise/InyxReborn/hxrrsilence Account. | Reason: Agent and his account.
   },
-  intelagent = {  
-   5515777500, -- Roblox Username: INTEL_DECLENSION | Demise/InyxReborn/hxrrsilence Group. | Reason: Agent.
+  NoNameIntelligenceAgent = {  
+   5515777500, -- Roblox Username: INTEL_DECLENSION | NoName Account Agent literally. | Reason: Agent, however, noname.
   },
+  UndercoverIntelligenceAgent = {  
+   5515777500, -- Roblox Username: INTEL_DECLENSION | Undercover Agent. | Reason: Agent, however, undercover agent as well.
+  },
+  IntelligenceAgent = {  
+   1874723110, -- Roblox Username: TheReaICalciumm | Demise/InyxReborn/hxrrsilence Account. | Reason: Agent and his account.
+   4067202176, -- Roblox Username: InyxReborn | Demise/InyxReborn/hxrrsilence Account. | Reason: Agent and his account.
+   2223734329, -- Roblox Username: OwnerByte | Demise/InyxReborn/hxrrsilence Account. | Reason: Agent and his account.
+   1418100044, -- Roblox Username: Kaspiianar | Kaspiianar Account. | Reason: Agent and his account.  
+   1396940578, -- Roblox Username: xy_nd | xy_nd Account. | Reason: Agent and his account.  
+  },  
 }
