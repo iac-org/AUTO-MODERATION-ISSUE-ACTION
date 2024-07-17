ae7b3defde50763a65bf8bb757316edd6a6c0cd9
Real <adrianusvir@gmail.com>
2024-07-17 22:23:19 +0200

    Create sa.lua

---
+++ sa.lua
@@ -0,0 +1,15 @@
+---INTERNATIONAL AUTHORITY COALITION. 
+
+--- ALL RIGHTS RESERVED. CONFIDENTIAL AND PROPRIETARY.
+--- IAC/sa.lua
+--- SPECIAL ACCOUNTS
+
+
+local Reports = {
+  alternative = {  
+   1885538965, --AmateurMastered | Alternative Account of the CelesticialFlaw
+  },
+  intelagent = {  
+   5515777500, --INTEL_DECLENSION
+  },
+}
