25cabc73ff2a8dc8890b962144bc00b62905ff90
Real <adrianusvir@gmail.com>
2024-07-18 18:07:08 +0200

    Create sa.lua

---
+++ sa.lua
@@ -0,0 +1,30 @@
+---INTERNATIONAL AUTHORITY COALITION.
+
+--- ALL RIGHTS RESERVED. CONFIDENTIAL AND PROPRIETARY.
+--- AUTO MODERATION ISSUE ACTION.
+--- AMIA: Special Accounts
+--- IAC/sa.lua
+--- SPECIAL ACCOUNTS
+
+
+local Reports = {
+    Alternative  = {
+        { userId = 1885538965, username = "AmateurMastered", reason = "Alternative Account of the CelesticialFlaw."},
+        { userId = 1874723110, username = "TheReaICalciumm", reason = "Alternative & Main of Demise/InyxReborn/hxrrsilence Account."},
+        { userId = 4067202176, username = "InyxReborn", reason = "Alternative & Main of Demise/InyxReborn/hxrrsilence Account."},
+    },
+    NoName = {
+        { userId = 5515777500, username = "INTEL_DECLENSION", reason = "No-Name Account Agent."},
+    },
+    Undercover = {
+        { userId = 5515777500, username = "INTEL_DECLENSION", reason = "Undercover Intelligence Account Agent."},
+    },
+    Intelligence = {
+        { userId = 1874723110, username = "TheReaICalciumm", reason = "Intelligence Agent."},
+        { userId = 4067202176, username = "InyxReborn", reason = "Intelligence Agent."},
+        { userId = 2223734329, username = "OwnerByte", reason = "Intelligence Agent."},
+        { userId = 1418100044, username = "Kaspiianar", reason = "Intelligence Agent."},
+        { userId = 1396940578, username = "xy_nd", reason = "Intelligence Agent."},
+        { userId = 1459696345, username = "Seytanelif", reason = "Intelligence Agent."},
+    },
+}
