e279a0e4d6340035b87d8a62067089bf752ebc54
Real <adrianusvir@gmail.com>
2024-07-18 18:22:17 +0200

    Create bg.lua

---
+++ bg.lua
@@ -0,0 +1,19 @@
+---INTERNATIONAL AUTHORITY COALITION. 
+
+--- ALL RIGHTS RESERVED. CONFIDENTIAL AND PROPRIETARY.
+--- AUTO MODERATION ISSUE ACTION.
+--- AMIA: Blacklisted Groups
+--- IAC/bg.lua
+--- BLACKLISTED GROUPS
+
+
+local Reports = {
+    BlacklistedGroups = {
+        { groupId = 34397025, groupName = "«« ₪ »»", reason = "Intelligence" },
+        { groupId = 34244533, groupName = "[NONEXISTENT]", reason = "Intelligence." },
+        { groupId = 34363884, groupName = "[SCPF — FEMIAS]", reason = "Intelligence." },
+        { groupId = 34244533, groupName = "[SCPF] • DEIMOS", reason = "Intelligence." },
+        { groupId = 33472020, groupName = "ː∎ː", reason = "Intelligence." },
+        { groupId = 14794327, groupName = "[ ARC-83 ]", reason = "Intelligence Foundation, possibly a threat.." },
+    },
+}
